5abf33ebd61e551ebd0314bad7c3cce1eaa794d4
adamhlomgern <146996877+adamhlomgern@users.noreply.github.com>
2024-10-09 14:23:59 +0200

    favicon - bugfix
    Fixade bugg där frågorna i min q&a-sektion inte öppnades på webbläsaren men fungerade perfekt i mobil.

--- script.js
+++ script.js
@@ -93,29 +93,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-
-// Script till FAQ
-
-// FAQ-klickfunktion för att expandera/dölja svaren
-document.querySelectorAll('.faq-question').forEach(item => {
-    item.addEventListener('click', () => {
-        const answer = item.nextElementSibling;
-
-        // Stänger alla andra öppna frågor
-        document.querySelectorAll('.faq-answer').forEach(otherAnswer => {
-            if (otherAnswer !== answer) {
-                otherAnswer.style.display = 'none';
-                otherAnswer.previousElementSibling.classList.remove('active');
-            }
-        });
-
-        // Visa eller dölj svaret beroende på dess nuvarande tillstånd
-        if (answer.style.display === 'block') {
-            answer.style.display = 'none';
-            item.classList.remove('active');
-        } else {
-            answer.style.display = 'block';
-            item.classList.add('active');
-        }
-    });
-});
